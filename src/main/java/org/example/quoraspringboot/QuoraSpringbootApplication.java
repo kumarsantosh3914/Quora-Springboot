f9ff31b86074c83113d580c24f68ad032d55ef93
Santosh Kumar <santoshkumar15novmth@gmail.com>
2026-02-06 18:30:05 +0530

    Added basic project setup

---
+++ src/main/java/org/example/quoraspringboot/QuoraSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.quoraspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoraSpringbootApplication.class, args);
+    }
+
+}
